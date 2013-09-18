3d543ae27796a16a865860a84c262249cd62690b
cabenitez <cabenitez83@gmail.com>
2013-09-18 13:33:16 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+var traductor = require('traductor');
+
+var dia = traductor.traducir_dia("Lunes", "en");
+var mes = traductor.traducir_mes("diciembre", "br");
+
+console.log(dia);
+console.log(mes);
